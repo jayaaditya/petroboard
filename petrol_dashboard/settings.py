ae3816c697e24b72674a618d88ede83bfab7446c
Jaya Aditya <jayaaditya@outlook.com>
2018-01-27 12:20:14 +0530

    change db creds

--- petrol_dashboard/settings.py
+++ petrol_dashboard/settings.py
@@ -76,15 +76,13 @@ WSGI_APPLICATION = 'petrol_dashboard.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 import database
 
-DATABASES = database.config()
-"""
+#DATABASES = database.config()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
